3ac5f8fc07d28dfc644c58d50eaf887e3b338da9
Shashank <shashankdobhal12@gmail.com>
2017-06-12 16:44:00 +0530

    initial commit

--- app/src/main/java/com/hfad/movieapp/adapter/MoviesAdapter.java
+++ app/src/main/java/com/hfad/movieapp/adapter/MoviesAdapter.java
@@ -1,7 +1,7 @@
 package com.hfad.movieapp.adapter;
 
 import android.content.Context;
-import android.graphics.Movie;
+import android.content.Intent;
 import android.support.v4.widget.SearchViewCompatIcs;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,8 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.hfad.movieapp.DetailActivity;
 import com.hfad.movieapp.R;
 import com.hfad.movieapp.model.Movie;
 
@@ -63,7 +65,21 @@ public class  MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHol
                 @Override
                 public  void onClick(View v){
                     int pos=getAdapterPosition();
-                    
+                    if(pos != RecyclerView.NO_POSITION){
+                        Movie clickedDataItem=movieList.get(pos);
+                        Intent intent=new Intent(mContext, DetailActivity.class);
+                        intent.putExtra("original_title", movieList.get(pos).getOriginalTitle());
+                        intent.putExtra("poster_path", movieList.get(pos).getPosterPath());
+                        intent.putExtra("overview", movieList.get(pos).getOverview());
+                        intent.putExtra("vote_average", movieList.get(pos).getvoteAverage());
+                        intent.putExtra("release_date", movieList.get(pos).getReleaseDate());
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        mContext.startActivity(intent);
+                        Toast.makeText(v.getContext(), "You Clicked"+clickedDataItem.getOriginalTitle(), Toast.LENGTH_SHORT).show();
+
+
+                    }
+
                 }
             });
         }
